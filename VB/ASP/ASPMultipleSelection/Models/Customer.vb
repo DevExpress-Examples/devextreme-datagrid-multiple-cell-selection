d8cce1c5a91f50ccd364dc0b5aab7094b470090d
DevExpressExampleBot <fakeEmail>
2022-12-20 12:05:38 +0400

    autogenerate VB

---
+++ VB/ASP/ASPMultipleSelection/Models/Customer.vb
@@ -0,0 +1,23 @@
+Namespace ASPMultipleSelection.Models
+
+    Public Class Customer
+
+        Public Property ID As Integer
+
+        Public Property CompanyName As String
+
+        Public Property Address As String
+
+        Public Property City As String
+
+        Public Property State As String
+
+        Public Property Zipcode As Integer
+
+        Public Property Phone As String
+
+        Public Property Fax As String
+
+        Public Property Website As String
+    End Class
+End Namespace
