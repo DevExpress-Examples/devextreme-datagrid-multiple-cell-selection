3f4e29b35fe0b9d74e4b5b73c66a2a08f59bf838
Charles Vincent Cordial (DevExpress) <charles.cordial@devsoft.ph>
2025-12-09 17:50:30 +0800

    Fix: Angular implmentation updated 25.1.3

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DxButtonModule } from 'devextreme-angular/ui/button';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    DxButtonModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, DxDataGridModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
