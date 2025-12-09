b5d452771dbac8e0466f5129bdb994badb35240a
Charles Vincent Cordial (DevExpress) <charles.cordial@devsoft.ph>
2025-12-09 17:49:15 +0800

    Fix: jQuery implmentation updated 25.1.3

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,29 @@
+let helper = null;
+
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
-    },
+  const dataGrid = $('#gridContainer')
+    .dxDataGrid({
+      dataSource: customers,
+      keyExpr: 'ID',
+      columns: ['CompanyName', 'City', 'State', 'Phone', 'Fax'],
+      showBorders: true,
+      onInitialized: (e) => {
+        if (!helper) {
+          helper = new SelectionHelper(e.component);
+        }
+      },
+    })
+    .dxDataGrid('instance');
+
+  $('#gridContainer').on('touchmove', (args) => {
+    // Attach a touchmove event to save the last touched cell
+    const event = args.touches[0];
+    const element = document.elementFromPoint(event.clientX, event.clientY);
+    const cellInfo = helper.cellsInfo.filter((x) => x.cellElement === element)[0];
+    if (cellInfo) {
+      helper.selectedRange.endRowIndex = cellInfo.rowIndex;
+      helper.selectedRange.endColumnIndex = cellInfo.columnIndex;
+      helper.showSelection(dataGrid, helper.selectedRange);
+    }
   });
 });
